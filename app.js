31e1950c43f1ad35b638ee3f2da72066809c776b
Makaan <nestor.figliuolo@gmail.com>
2018-06-28 20:19:36 -0300

    probando api mixcloud

---
+++ app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    var app = new Vue({
+        el: '#app',
+        data: {
+            programas: []
+        }
+    })
+
+    $.get("https://api.mixcloud.com/radiopixel/cloudcasts/", function(res) {
+
+
+        let i = 0;
+        JSON.parse(res).data.forEach(element => {
+            $.get('http://api.mixcloud.com' + element.key + 'embed-json/', function(res) {
+
+                app.programas.push({
+                    'i': i,
+                    'html': JSON.parse(res).html
+                });
+                if (i == 20) {
+                    app.programas.sort((a, b) => a.i < b.i);
+                }
+            });
+            i++;
+        });
+
+    })
+
+
+})
